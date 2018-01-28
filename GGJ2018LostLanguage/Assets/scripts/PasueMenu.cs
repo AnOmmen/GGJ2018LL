e41aa0b60cdf0b092aa3d04ad7b5ee93f3592beb
CollinOmmen <collin.p.ommen@gmail.com>
2018-01-28 14:32:03 -0500

    Package Import

---
+++ GGJ2018LostLanguage/Assets/scripts/PasueMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasueMenu : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+
+    public GameObject Menu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Menu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        Menu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+        Debug.Log("Loading menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
---
+++ GGJ2018LostLanguage/Assets/scripts/Play.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void LoadByIndex(int scenelvl)
+    {
+        SceneManager.LoadScene(scenelvl);
+    }
+}
